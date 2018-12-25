e29c8c36af46eccbdb07a4cc76982fc0e74a16a7
@tshemake <tshemake@gmail.com>
2018-12-25 15:53:13 +0500

    Add Serilog

--- PhoneBook.API/Program.cs
+++ PhoneBook.API/Program.cs
@@ -5,17 +5,35 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Logging;
+using Serilog;
 
 namespace PhoneBook.API
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static int Main(string[] args)
         {
-            CreateWebHostBuilder(args).Build().Run();
+             try
+            {
+                BuildWebHost(args).Run();
+                return 0;
+            }
+            catch
+            {
+                return 1;
+            }
         }
+
+        public static IWebHost BuildWebHost(string[] args) =>
+            CreateWebHostBuilder(args)
+                .ConfigureLogging((context, logging) =>
+                {
+                    logging.AddSerilog(
+                        new LoggerConfiguration()
+                            .ReadFrom.Configuration(context.Configuration)
+                            .CreateLogger());
+                })
+                .Build();
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
--- PhoneBook.API/Startup.cs
+++ PhoneBook.API/Startup.cs
@@ -8,8 +8,6 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.ApiExplorer;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
 using PhoneBook.API.Models;
 using Swashbuckle.AspNetCore.Swagger;
 
